7e01bc30930e557cd72af23ce3a64f915d93b030
Jingu Kang <jinguk0816@gmail.com>
2021-07-20 12:46:37 -0400

    urlDecoder to make url string into an object format

---
+++ urlDecode.js
@@ -0,0 +1,32 @@
+/**
+ * To safely send data in a URL, the data first has to be encoded to convert any special characters to URL safe characters.
+ * For this assignment we will only focus on the following URL encoding rules:
+  *  %20 represents a space character.
+  *  Key-value pairs are represented using an = character: key=value
+  *  Multiple key-value pairs are separated using a & character: key1=value1&key2=value2
+ 
+* Example Input = city=Vancouver&weather=lots%20of%20rain
+
+* Example Output
+{
+  city: "Vancouver",
+  weather: "lots of rain"
+}
+ */
+
+const urlDecode = function(text) {
+  const returnObj = {};
+  
+  const mapped = text.split("&").map(type => type.split("="));
+
+  for (let i = 0; i < mapped.length; i++) {
+    returnObj[mapped[i][0]] = mapped[i][1].split("%20").join(" ");
+  }
+
+  return returnObj;
+};
+
+console.log(urlDecode("duck=rubber"));
+console.log(urlDecode("bootcamp=Lighthouse%20Labs"));
+console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain"));
+console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain").weather);
