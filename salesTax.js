d3903691c00805be1660f2ac97d82983ec627ea5
Jingu Kang <jinguk0816@gmail.com>
2021-07-27 12:46:23 -0400

    program that calculates tax for provinces in Canada and outputs in object format

--- salesTax.js
+++ salesTax.js
@@ -35,48 +35,23 @@ const companySalesData = [
     sales: [ 500, 100 ]
   }
 ];
-//Helper function that calculates the total tax amount
-const taxCalculator = (sales, taxRate) => {
-  return sales * taxRate;
-};
 
 const calculateSalesTax = (salesData, taxRates) => {
-  //Calculates totalSales of each object from the input
-  let totalSales = salesData.map((obj) => {
-    return obj.sales.reduce((acc, curr) => acc + curr);
-  });
-  //Loops through the key of tax rates in the 'taxRates' variable
-  for (let state in taxRates) {
-    //Loops through salesData object input objects
-    for (let i = 0; i < salesData.length; i++) {
-      //converts sales of each object into a single 'totalSales'
-      salesData[i].sales = totalSales[i]; // ******
-      //checks if the state of taxrate variable matches the current looping object's province
-      if (state === salesData[i].province) {
-        //If it matches, assign new key 'tax' to the object and assign total tax as a value
-        salesData[i]['tax'] = taxCalculator(salesData[i].sales, taxRates[state]); // ******
-      }
-    }
-  }
-  //Initialize final return object
+  
   const returnObj = {};
   
-  //loops through objects of input object
-  for (let obj of salesData) {
-    //Checks if the result output already has current looping property
-    if (!returnObj[obj.name]) {
-      //If not, create an object and add it to the return object
-      returnObj[obj.name] = {
-        totalSales: obj.sales,
-        totalTaxes: obj.tax
+  for (let {name, province, sales} of salesData) {
+    if (!returnObj[name]) {
+      returnObj[name] = {
+        totalSales: 0,
+        totalTaxes: 0
       };
-      //If yes, add on below values
-    } else {
-      returnObj[obj.name].totalSales += obj.sales;
-      returnObj[obj.name].totalTaxes += obj.tax;
     }
+    let totalSales = sales.reduce((acc, curr) => acc + curr);
+    returnObj[name].totalSales += totalSales;
+    returnObj[name].totalTaxes += taxRates[province] * totalSales;
   }
-  //return final object
+  
   return returnObj;
 };
 
